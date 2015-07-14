4c61fc1fe4ab5e582ea55ae27e2e20c9cc4db267
Michael Peters <mtpeters@us.ibm.com>
2015-07-14 12:59:50 -0400

    upgrade for beta5

--- src/dotnetstarter/Startup.cs
+++ src/dotnetstarter/Startup.cs
@@ -1,25 +1,35 @@
 ﻿using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
 using Microsoft.Framework.DependencyInjection;
-using Microsoft.AspNet.StaticFiles;
+using Microsoft.Framework.Logging;
+using Microsoft.Framework.Configuration;
+using Microsoft.Framework.Runtime;
 
 public class Startup
 {
+	public IConfiguration Configuration { get; set; }
+
+	public Startup(IHostingEnvironment env, IApplicationEnvironment appEnv)
+	{
+		var configBuilder = new ConfigurationBuilder(appEnv.ApplicationBasePath)
+			.AddJsonFile("config.json")
+			.AddEnvironmentVariables();
+		Configuration = configBuilder.Build();
+	}
 
 	public void ConfigureServices(IServiceCollection services)
 	{
 		services.AddMvc();
 	}
 
-	public void Configure(IApplicationBuilder app)
+	public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
 	{
-		app.UseFileServer(new FileServerOptions()
-        {
-            EnableDirectoryBrowsing = false,
-        });
+		loggerFactory.AddConsole();
 
-		app.UseMvc(routes =>
-		{
-			routes.MapRoute("default", "{controller=Home}/{action=Index}/{id?}");
-        });
+		app.UseErrorPage();
+
+		app.UseStaticFiles();
+
+		app.UseMvcWithDefaultRoute();
     }
 }
