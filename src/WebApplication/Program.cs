54de751eaa6b0f88d3f978e8d6900057470f9f0f
Daniel Grim <degrim@us.ibm.com>
2017-01-27 15:11:03 -0500

    Use MVC

---
+++ src/WebApplication/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Hosting;
+
+namespace WebApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .AddCommandLine(args)
+                .Build();
+
+            var host = new WebHostBuilder()
+                        .UseKestrel()
+                        .UseConfiguration(config)
+                        .UseStartup<Startup>()
+                        .Build();
+            host.Run();
+        }
+    }
+}
